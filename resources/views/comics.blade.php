ef175a6525b3e22c709a88d0f8ba4c514e345e8e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-01 17:23:54 +0200

    Added structure of comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,5 +4,23 @@
     
 
 @section('content-main')
-    <h1>Comics</h1>    
+    <section id="content">
+        <!-- Spotlight -->
+        <div class="spotlight bg-style w-100"></div>
+            <!-- Comic Book section container -->
+            <article class="comics-container container">
+                <button class="btn-series clickable">CURRENT SERIES</button>
+                <ul class="d-flex f-wrap">
+                   inserisco contenuto
+                </ul>
+                <div class="btn-content d-flex j-content-center w-100">
+                    <button class="clickable">LOAD MORE</button>
+                </div>
+        </article>
+    </section>
+    <section id="panel-content">
+        <ul class="container d-flex j-content-around a-items-center">
+          inserisco contenuto
+        </ul>
+      </section>
 @endsection
